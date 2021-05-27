7999a28ec1f48acaddd88569969dcbc57d9acc01
AndresCarrenoP <andres.carreno@live.com>
2021-05-26 21:16:30 -0500

    Update TestNGexercise1 to work with multithread

--- src/test/java/module3/TestNGexercise1.java
+++ src/test/java/module3/TestNGexercise1.java
@@ -1,13 +1,40 @@
 package module3;
 
+import common.BaseTestMultiThread;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import common.BaseTest;
 
-public class TestNGexercise1 extends BaseTest {
+public class TestNGexercise1 extends BaseTestMultiThread {
 
-    @Test (groups = {"sanity"})
+    @Test(groups = {"sanity"})
     public void testGoogleNavigate() {
+
+        final String googleURL = "https://www.google.com.mx/";
+        this.getDriver().get(googleURL);
+
+        String currentURL = this.getDriver().getCurrentUrl();
+
+        Assert.assertEquals(currentURL, googleURL, "Wrong URL");
+
+    }
+
+
+    @Test(groups = {"sanity"})
+    public void testGoogleNavigate2() {
+
+        final String googleURL = "https://www.google.com.mx/";
+        this.getDriver().get(googleURL);
+
+        String currentURL = this.getDriver().getCurrentUrl();
+
+        Assert.assertEquals(currentURL, googleURL, "Wrong URL");
+
+    }
+
+
+    @Test(groups = {"sanity"})
+    public void testGoogleNavigate3() {
 
         final String googleURL = "https://www.google.com.mx/";
         this.getDriver().get(googleURL);
